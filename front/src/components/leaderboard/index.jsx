75e935eb4e82fc09e89dee1a54cb0dfd6fe2bd9d
Adil <adilzhapar2002@gmail.com>
2022-07-07 20:41:56 +0600

    Aidar

--- front/src/components/leaderboard/index.jsx
+++ front/src/components/leaderboard/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './index.css';
 import top1 from '../../img/top1.svg';
+import Aidar from '../../img/Aidar_0.png';
 
 
 const Leaderboard = () => {
@@ -14,7 +15,7 @@ const Leaderboard = () => {
                     <p id="slogan"> TOP-10 participants</p>
                 </div>
             </div>
-            
+            <img src={Aidar} alt="Aidar" style={{"width": "50vw", "marginLeft": "20px"}}/>
         </div>
     );
 }
