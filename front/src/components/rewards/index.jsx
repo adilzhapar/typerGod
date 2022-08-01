054c37ceb61e6beac5ddf4f8b9bc9df4abc54bf9
Adil <adilzhapar2002@gmail.com>
2022-08-01 18:25:01 +0600

    integration of nft 50%

--- front/src/components/rewards/index.jsx
+++ front/src/components/rewards/index.jsx
@@ -10,11 +10,63 @@ import { setPending } from '../../features/pendingSlice';
 import abi from "../../utils/TyperGod.json";
 import { ethers } from "ethers";
 import axios from "axios";
+import nft1 from '../../img/NFTs/1.png';
+import nft2 from '../../img/NFTs/2.png';
+import nft3 from '../../img/NFTs/3.png';
+import nft4 from '../../img/NFTs/4.png';
+import nft5 from '../../img/NFTs/5.png';
+import nft6 from '../../img/NFTs/6.png';
+import nft7 from '../../img/NFTs/7.png';
 
 
 const BASE_URL = "https://typer-god.herokuapp.com";
 // const BASE_URL = "http://localhost:8080";
 
+
+const nftCards = [
+    {
+        "name": "Malevich #1",
+        "link": "https://gateway.pinata.cloud/ipfs/QmSZi3z2WYkTJn2xdW6Sq1nrtGBbBSM34pQ15MfRLVWm9k",
+        "price": 100,
+        "img": nft1
+    },
+    {
+        "name": "Aivazovsky #1",
+        "link": "https://gateway.pinata.cloud/ipfs/Qmaji6eFq5NFu5cWiBYjKcKcSSg4Aq6fkFkUEquBLRbaDD",
+        "price": 200,
+        "img": nft2
+    },
+    {
+        "name": "van Gogh #1",
+        "link": "https://gateway.pinata.cloud/ipfs/QmeQiuqXQr9PXp7fi2bJYbzusmagGctEdy7kWPmJ61UnS7",
+        "price": 400,
+        "img": nft3
+    },
+    {
+        "name": "Aivazovsky #2",
+        "link": "https://gateway.pinata.cloud/ipfs/QmZ6SUAiUGShZwNRngd8fGhiVKfefGrv9NAnPFyXHDuwLV",
+        "price": 800,
+        "img": nft4
+    },
+    {
+        "name": "Da Vinci #1",
+        "link": "https://gateway.pinata.cloud/ipfs/QmT7ut7J551cF5jAWwmVQG1kthVDsTzMo1mJ8WydtLP7LF",
+        "price": 1500,
+        "img": nft5
+    },
+    {
+        "name": "van Gogh #2",
+        "link": "https://gateway.pinata.cloud/ipfs/QmdBh2K7FmPafX5uCLMr9fEFqn2WcrNBzKM6DmCLDrbEvY",
+        "price": 3000,
+        "img": nft6
+    },
+    {
+        "name": "Michelangelo #1",
+        "link": "https://gateway.pinata.cloud/ipfs/QmahkL8o1xDCrh84QrudYy9hCxiuipSFeTPVQAE46pH52v",
+        "price": 5000,
+        "img": nft7
+    }
+]
 
 
 const Rewards = () => {
@@ -23,6 +75,7 @@ const Rewards = () => {
     const [onChain, setOnChain] = useState(0);
     const dispatch = useDispatch();
     const currentAccount = useSelector((state) => state.currentAccount.value);
+    const [nftItems, setNftItems] = useState(nftCards);
 
     const contractAddress = "0xd3F1319F7b50a8ea22A36F7A2625d44310aeebf5";
 
@@ -89,7 +142,7 @@ const Rewards = () => {
                     handleOnChain();
 
                     let WpmSum, attempts, highscore, img;
-                    
+
 
                     const newData = await axios.get(`${BASE_URL}/users/${currentAccount}`);
                     WpmSum = newData.data[0].WpmSum;
@@ -178,6 +231,37 @@ const Rewards = () => {
                 <div className="send-button">
                     <button id="send" onClick={handleTransaction}><img src={send} alt="send" style={{ "marginTop": "0.5vh" }} /></button>
                 </div>
+            </div>
+
+            <div className="rewards-nft">
+                {nftItems.map((item, index) => (
+                    <div className="rewards-nft-object" key={index}>
+                        <div
+                            className="rewards-nft-object-div1"
+                            style={{ backgroundImage: `url(${item.img})` }}>
+                            <h2>{item.name}</h2>
+                            <p className="rewards-nft-object-div1-p">{item.price} on-chain tokens required</p>
+                        </div>
+{/* YOU STOPPED HERE, START FROM INTEGRATING HARDHAT */}
+                        {   
+                            // owned.includes(item.link) ? (
+                            //     <div className="rewards-nft-object-div2">
+                            //         <button className="rewards-nft-object-btn2">Already owned</button>
+                            //     </div>
+                            // ) :
+                            item.price <= pending ? (
+                                <div className="rewards-nft-object-div2">
+                                    <button className="rewards-nft-object-btn">Mint NFT</button>
+                                </div>
+                            ) : (
+                                <div className="rewards-nft-object-div2">
+                                    <button className="rewards-nft-object-btn2">Insufficient TGT tokens</button>
+                                </div>
+                            )
+                        }
+
+                    </div>
+                ))}
             </div>
 
 
